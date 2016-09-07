b05f694f753055fd3b25c9d4477237c197b72eb0
rhys <rbrooker105@gmail.com>
2016-09-07 20:45:37 +1000

    divided array

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,8 +1,12 @@
-def merge_sort
+def merge_sort(array)
+left, right = divide(array)
 end
 
-def divide
-	
+def divide(array)
+  divide_point = (array.length / 2)
+  divide_point += 1 if (array.length % 2 != 0) #left will be the bigger array if elements is odd
+  left,right = array.each_slice(divide_point).to_a
+  return left, right
 end
 
 def sort
