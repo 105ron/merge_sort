1543a227437210559461a9859edf22c7aa8e1de3
rhys <rbrooker105@gmail.com>
2016-09-08 14:09:38 +1000

    working sort

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,6 +1,11 @@
 def merge_sort(array)
-left, right = divide(array)
+  return array if (array.length) > 1
+  left, right = divide(array)
+  merge_sort(left)
+  merge_sort(right)
+  merge(left,right,array)
 end
+
 
 def divide(array)
   divide_point = (array.length / 2)
@@ -9,8 +14,34 @@ def divide(array)
   return left, right
 end
 
-def sort
+
+def merge(left,right,array)
+  left_index = 0
+  right_index = 0
+  array_index = 0
+  left_length = left.length - 1
+  right_length = right.length - 1
+  while (left_index <= left_length) && (right_index <= right_length)
+  	if (left[left_index]) < (right[right_index])
+  	  array[array_index] = left[left_index]
+  	  left_index += 1
+  	else
+  	  array[array_index] = right[right_index]
+  	  right_index += 1
+  	end
+  array_index += 1
+  end
+
+  while (left_index <= left_length)
+	array[array_index] = left[left_index]
+  	left_index += 1
+  	array_index +=1
+  	end
+  while (right_index <= right_length)
+	array[array_index] = right[right_index]
+  	right_index += 1
+  	array_index +=1
+  	end
 end
 
-def combine
-end
+puts merge_sort([1,3,5,6,2,4,7,8,9])
